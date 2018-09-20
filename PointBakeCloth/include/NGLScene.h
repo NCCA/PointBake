bc460ca2f38069f4e9f9ec87b3283a97015fbc14
jmacey <jmacey@bournemouth.ac.uk>
2018-09-20 12:04:24 +0100

    updated for linux build

--- PointBakeCloth/include/NGLScene.h
+++ PointBakeCloth/include/NGLScene.h
@@ -1,10 +1,10 @@
 #ifndef NGLSCENE_H_
 #define NGLSCENE_H_
-#include <QOpenGLWindow>
 #include <ngl/Obj.h>
 #include <ngl/NCCAPointBake.h>
 #include <ngl/Text.h>
 #include "WindowParams.h"
+#include <QOpenGLWindow>
 #include <memory>
 
 //----------------------------------------------------------------------------------------------------------------------
